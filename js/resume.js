5ce500148eff9c0a86409b0a477e217f03fdfdb6
Bradley Trager <bradleytrager@Bradleys-MacBook-Pro.local>
2013-11-16 19:31:08 -0500

    added add/remove functionality

--- js/resume.js
+++ js/resume.js
@@ -1,88 +1,109 @@
+var NEW_SECTION = {
+	title: 'New Section',
+	items: [{
+		title: "Title",
+		location: "Location",
+		date: "Date",
+		details: [{
+			detail: "Detail 1"
+		}]
+	}]
+};
+var NEW_ITEM = {
+	title: "Title",
+	location: "Location",
+	date: "Date",
+	details: [{
+		detail: "Detail 1"
+	}]
+};
+NEW_DETAIL = {
+	detail: "Detail 1"
+};
 var RESUME = {
 	name: "Bradley Trager",
 	title: "Web Developer",
-	email:"btrager@softfinity.com",
-	website:"http://www.softfinity.com",
-	phone:"516-456-1277",
-	address1:"6665 Endinborough Dr",
-	address2:"West Bloomfield, MI 48322",
-	summary:"Web Developer specializing in single-page web applications (SPA) and content management systems (CMS) using several back-end technologies including php, asp.net, java, ruby and nodejs.",
+	email: "btrager@softfinity.com",
+	website: "http://www.softfinity.com",
+	phone: "516-456-1277",
+	address1: "6665 Endinborough Dr",
+	address2: "West Bloomfield, MI 48322",
+	summary: "Web Developer specializing in single-page web applications (SPA) and content management systems (CMS) using several back-end technologies including php, asp.net, java, ruby and nodejs.",
 	sections: [{
-			title: 'Experience',
-			items: [{
-					title: "Senior Web Developer",
-					location: "Team Detroit",
-					date: "September 2013-present",
-					details: [{
-						detail: "Web Developer specializing in single-page web applications (SPA) and content management systems (CMS) using several back-end technologies including php, asp.net, java, ruby and nodejs."
-					}]
-				}, {
-					title: "Senior Web Developer",
-					location: "SoftFinity",
-					date: "October 2012-present",
-					details: [{
-						detail: "Develop single page JavaScript web applications with various javscript frameworks"
-					}, {
-						detail: "Develop solutions for storage, analysis and visualization of big data"
-					}, {
-						detail: "Write articles about web development that have attracted over 24,000 unique visitors to our blog"
-					}, {
-						detail: "Mentoring Intern Software Developer"
-					}]
-				}, {
-					title: "C++ Programmer",
-					location: "Cornell University",
-					date: "August 2005- May 2007",
-					details: [{
-							detail: "Created finite-element simulations using C++ and MatLab"}]
-					}]
-			},
-			{
-				title: 'Education',
-				items: [{
-					title: "Master of Engineering in Applied Physics",
-					location: "Cornell University, College of Engineering",
-					date: "August 2002 - May 2007",
-					details:[]
-				}, {
-					title: "Bachelor of Religious Studies",
-					location: "Rabbinical College of America, Mayanot Institute",
-					date: "June 2007 - January 2012",
-					details:[]
-				}]
-			},
-			{
-				title: "Skills",
-				items: [{
-					title: "Javascript",
-					details: [{
-						detail: "jQuery, jQuery UI, jQuery Mobile, Knockout.js, Ember.js, Backbone.js, Angular.js"
-					}]
-				}, {
-					title: "Content Management Systems",
-					details: [{
-						detail: "Concrete5, Wordpress, Magento"
-					}]
-				}, {
-					title: "Server-Side Frameworks",
-					details: [{
-						detail: "PHP, ASP.NET MVC, C#, Java, Node.js, Ruby"
-					}]
-				}, {
-					title: "Databases",
-					details: [{
-						detail: "MySQL, SQL Server, Oracle, MongoDB"
-					}]
-				}, {
-					title: "CSS",
-					details: [{
-						detail: "Sass, Less, Compass, Bootstrap, Zurb Foundation"
-					}]
-				}, {
-					title: "Tools",
-					details: [{
-						detail: "Git, SVN, Google Webmaster Tools, Google Analytics, Jira, Photoshop"
-					}]
-				}]
+		title: 'Experience',
+		items: [{
+			title: "Senior Web Developer",
+			location: "Team Detroit",
+			date: "September 2013-present",
+			details: [{
+				detail: "Web Developer specializing in single-page web applications (SPA) and content management systems (CMS) using several back-end technologies including php, asp.net, java, ruby and nodejs."
 			}]
-	};
+		}, {
+			title: "Senior Web Developer",
+			location: "SoftFinity",
+			date: "October 2012-present",
+			details: [{
+				detail: "Develop single page JavaScript web applications with various javscript frameworks"
+			}, {
+				detail: "Develop solutions for storage, analysis and visualization of big data"
+			}, {
+				detail: "Write articles about web development that have attracted over 24,000 unique visitors to our blog"
+			}, {
+				detail: "Mentoring Intern Software Developer"
+			}]
+		}, {
+			title: "C++ Programmer",
+			location: "Cornell University",
+			date: "August 2005- May 2007",
+			details: [{
+				detail: "Created finite-element simulations using C++ and MatLab"
+			}]
+		}]
+	}, {
+		title: 'Education',
+		items: [{
+			title: "Master of Engineering in Applied Physics",
+			location: "Cornell University, College of Engineering",
+			date: "August 2002 - May 2007",
+			details: []
+		}, {
+			title: "Bachelor of Religious Studies",
+			location: "Rabbinical College of America, Mayanot Institute",
+			date: "June 2007 - January 2012",
+			details: []
+		}]
+	}, {
+		title: "Skills",
+		items: [{
+			title: "Javascript",
+			details: [{
+				detail: "jQuery, jQuery UI, jQuery Mobile, Knockout.js, Ember.js, Backbone.js, Angular.js"
+			}]
+		}, {
+			title: "Content Management Systems",
+			details: [{
+				detail: "Concrete5, Wordpress, Magento"
+			}]
+		}, {
+			title: "Server-Side Frameworks",
+			details: [{
+				detail: "PHP, ASP.NET MVC, C#, Java, Node.js, Ruby"
+			}]
+		}, {
+			title: "Databases",
+			details: [{
+				detail: "MySQL, SQL Server, Oracle, MongoDB"
+			}]
+		}, {
+			title: "CSS",
+			details: [{
+				detail: "Sass, Less, Compass, Bootstrap, Zurb Foundation"
+			}]
+		}, {
+			title: "Tools",
+			details: [{
+				detail: "Git, SVN, Google Webmaster Tools, Google Analytics, Jira, Photoshop"
+			}]
+		}]
+	}]
+};
